fb65d7b6ea8fdba2992e79da7bc29a61a6ce10cc
choseongwon <qqwow741@gmail.com>
2020-01-09 17:44:43 +0900

    add state

--- src/Components/Main/App.tsx
+++ src/Components/Main/App.tsx
@@ -17,6 +17,11 @@ class App extends React.Component<any, any>{
         r_pwd: '',
         email: '',
 
+        id_text: '',
+        email_text: '',
+        pwd_text: '',
+        r_pwd_text: '',
+
         id_border: 'white',
         email_border: 'white',
         pwd_border: 'white',
@@ -27,12 +32,6 @@ class App extends React.Component<any, any>{
         const pwd_regex = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
         const email_regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-        let id_text = "영어와 숫자로 5~12자 만들어주세요.";
-        let email_text = "이메일 형식에 맞지않습니다.";
-        let pwd_text = "영어와 숫자, 특수문자로 8~16자를 만들어주세요.";
-        let r_pwd_text = "비밀번호가 일치하지 않습니다.";
-
-
 
         const onChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
             this.setState({
@@ -41,49 +40,55 @@ class App extends React.Component<any, any>{
         };
 
         const onFocus1 = (e: React.FocusEvent<HTMLInputElement>) => {
-            console.log(id_regex.test(this.state.id))
             if (!id_regex.test(this.state.id)) {
                 this.setState({
-                    id_border: 'red'
+                    id_border: 'red',
+                    id_text: '영어와 숫자로 5~12자 만들어주세요.'
                 })
             }
             if (id_regex.test(this.state.id)) {
                 this.setState({
-                    id_border: 'white'
+                    id_border: 'white',
+                    id_text: ''
                 })
-                id_text = "";
             }
 
             if (!pwd_regex.test(this.state.pwd)) {
                 this.setState({
-                    pwd_border: 'red'
+                    pwd_border: 'red',
+                    pwd_text: '영어와 숫자, 특수문자로 8~16자를 만들어주세요.'
                 })
             }
             if (pwd_regex.test(this.state.pwd)) {
                 this.setState({
-                    pwd_border: 'white'
+                    pwd_border: 'white',
+                    pwd_text: ''
                 })
             }
 
             if (!email_regex.test(this.state.email)) {
                 this.setState({
-                    email_border: 'red'
+                    email_border: 'red',
+                    email_text: '이메일 형식에 맞지않습니다.'
                 })
             }
             if (email_regex.test(this.state.email)) {
                 this.setState({
-                    email_border: 'white'
+                    email_border: 'white',
+                    email_text: ''
                 })
             }
 
             if (this.state.pwd !== this.state.r_pwd) {
                 this.setState({
-                    r_pwd_border: 'red'
+                    r_pwd_border: 'red',
+                    r_pwd_text: '비밀번호가 일치하지 않습니다.'
                 })
             }
             if(this.state.pwd === this.state.r_pwd){
                 this.setState({
-                    r_pwd_border: 'white'
+                    r_pwd_border: 'white',
+                    r_pwd_text: ''
                 })
             }
         };
@@ -91,47 +96,53 @@ class App extends React.Component<any, any>{
         const onBlur1 = (e: React.FocusEvent<HTMLInputElement>) => {
             if (!id_regex.test(this.state.id)) {
                 this.setState({
-                    id_border: 'red'
+                    id_border: 'red',
+                    id_text: '영어와 숫자로 5~12자 만들어주세요.'
                 })
             }
             if (id_regex.test(this.state.id)) {
                 this.setState({
-                    id_border: 'white'
+                    id_border: 'white',
+                    id_text: ''
                 })
             }
 
             if (!pwd_regex.test(this.state.pwd)) {
                 this.setState({
-                    pwd_border: 'red'
+                    pwd_border: 'red',
+                    pwd_text: '영어와 숫자, 특수문자로 8~16자를 만들어주세요.'
                 })
             }
             if (pwd_regex.test(this.state.pwd)) {
                 this.setState({
-                    pwd_border: 'white'
+                    pwd_border: 'white',
+                    pwd_text: ''
                 })
             }
 
             if (!email_regex.test(this.state.email)) {
                 this.setState({
-                    email_border: 'red'
+                    email_border: 'red',
+                    email_text: '이메일 형식에 맞지않습니다.'
                 })
             }
             if (email_regex.test(this.state.email)) {
                 this.setState({
-                    email_border: 'white'
+                    email_border: 'white',
+                    email_text: ''
                 })
             }
 
-
-
             if (this.state.pwd !== this.state.r_pwd) {
                 this.setState({
-                    r_pwd_border: 'red'
+                    r_pwd_border: 'red',
+                    r_pwd_text: '비밀번호가 일치하지 않습니다.'
                 })
             }
             if(this.state.pwd === this.state.r_pwd){
                 this.setState({
-                    r_pwd_border: 'white'
+                    r_pwd_border: 'white',
+                    r_pwd_text: ''
                 })
             }
         };
@@ -167,13 +178,13 @@ class App extends React.Component<any, any>{
                             <h1 className={'form_logo'}>가입하기</h1>
                             <form>
                                 <input type={'text'} className={'register_input'} style={{borderColor: this.state.id_border}} name={'id'} id={'id'} placeholder={'아이디'} onChange={onChange1} onFocus={onFocus1} onBlur={onBlur1}/>
-                                <span style={{fontSize: 10, color: 'red'}}>{id_text}</span>
+                                <span style={{fontSize: 10, color: 'red'}}>{this.state.id_text}</span>
                                 <input type={'text'} className={'register_input'} style={{ borderColor: this.state.email_border }} name={'email'} id={'email'} placeholder={'이메일'} onChange={onChange1} onFocus={onFocus1} onBlur={onBlur1}/>
-                                <span style={{fontSize: 10, color: 'red'}}>{email_text}</span>
+                                <span style={{fontSize: 10, color: 'red'}}>{this.state.email_text}</span>
                                 <input type={'password'} className={'register_input'} style={{ borderColor: this.state.pwd_border }} name={'pwd'} id={'pwd'} placeholder={'비밀번호'} onChange={onChange1} onFocus={onFocus1} onBlur={onBlur1}/>
-                                <span style={{fontSize: 10, color: 'red'}}>{pwd_text}</span>
+                                <span style={{fontSize: 10, color: 'red'}}>{this.state.pwd_text}</span>
                                 <input type={'password'} className={'register_input'} style={{ borderColor: this.state.r_pwd_border }} name={'r_pwd'} id={'r_pwd'} placeholder={'비밀번호 확인'} onChange={onChange1} onFocus={onFocus1} onBlur={onBlur1}/>
-                                <span style={{fontSize: 10, color: 'red'}}>{r_pwd_text}</span>
+                                <span style={{fontSize: 10, color: 'red'}}>{this.state.r_pwd_text}</span>
                                 <input type={'submit'} className={'register_button'}  id={'register'} value={'가입'} onClick={send} />
                             </form>
 
